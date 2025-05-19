e729c9e12d930bd3503d3a2862dc51f4c9439c37
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-19 10:55:15 +0200

    Remove starter code

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -1,14 +1,8 @@
 import { Form } from 'react-router';
-import type { Route } from '../../routes/notes/+types/new';
 import Button from '~/components/ui/Button';
 import Card from '~/components/ui/Card';
 import Input from '~/components/ui/Input';
 import TextArea from '~/components/ui/TextArea';
-
-export async function action({ request }: Route.ActionArgs) {
-  console.log(request);
-  throw new Error('This route is not implemented yet.');
-}
 
 export default function NewNoteRoute() {
   return (
